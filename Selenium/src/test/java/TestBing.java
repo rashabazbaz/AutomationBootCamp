6869b144ec09eb9c76d359b6be4e2d1bab1adc94
rashabazbaz <111975092+rashabazbaz@users.noreply.github.com>
2022-09-12 18:48:57 +0300

    findElements

--- Selenium/src/test/java/TestBing.java
+++ Selenium/src/test/java/TestBing.java
@@ -1,10 +1,12 @@
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class TestBing {
-	
+
 	@Test
 	public void testBing() throws InterruptedException {
 		WebDriver driver = OpenBrowsers.openBrowser("chrome");
@@ -12,15 +14,20 @@ public class TestBing {
 		Thread.sleep(10000);
 		WebElement search = driver.findElement(By.name("q"));
 		search.sendKeys("Selenium");
-		
-		search.clear();
-		search.sendKeys("Celin dion");
+		//		
+		//		search.clear();
+		//		search.sendKeys("Celin dion");
 		WebElement btn = driver.findElement(By.xpath("//*[@id=\"search_icon\"]"));
 		btn.click();
-		WebElement firstResult = driver.findElement(By.xpath("//*[@id=\"b_results\"]//*/h2/a[1]")); 
-		System.out.println(firstResult.getText());
+		List<WebElement> results = driver.findElements(By.xpath("//*[@id=\"b_results\"]//*/h2/a")); 
+		for(WebElement result: results) {
+			System.out.println(result.getText());
+			System.out.println(result.getAttribute("href"));
+		}
 		
+		driver.quit();
+		//driver.switchTo().alert().accept();
 		System.out.println("Test");
-		
+
 	}
 }
