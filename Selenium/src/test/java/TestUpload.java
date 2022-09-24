550f5e31a6cd46da9566def5b0b525d235926e6d
rashabazbaz <111975092+rashabazbaz@users.noreply.github.com>
2022-09-24 11:51:09 +0300

    Upload

--- Selenium/src/test/java/TestUpload.java
+++ Selenium/src/test/java/TestUpload.java
@@ -1,4 +1,23 @@
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import Core.OpenBrowsers;
 
 public class TestUpload {
-
+	@Test
+	public void testUpload() {
+		WebDriver driver = OpenBrowsers.openBrowser("chrome");
+		driver.manage().window().maximize();
+		driver.get("https://demo.guru99.com/test/upload/");
+		File file = new File("input.csv");
+		driver.findElement(By.name("uploadfile_0")).sendKeys(file.getAbsolutePath());
+		
+		driver.findElement(By.id("submitbutton")).click();
+		driver.findElement(By.id("terms")).click();
+		
+		
+	}
 }
