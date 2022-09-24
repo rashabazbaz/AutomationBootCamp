4b7ec8d6ade21179899f0f06d5b29fca76a8cb19
rashabazbaz <111975092+rashabazbaz@users.noreply.github.com>
2022-09-24 09:57:21 +0300

    options

--- Selenium/src/main/java/Core/OpenBrowsers.java
+++ Selenium/src/main/java/Core/OpenBrowsers.java
@@ -1,10 +1,65 @@
 package Core;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
 
 public class OpenBrowsers {
+	public static WebDriver openchromeWithOptions() {
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		String downloadFilepath = "downloads";
+		File file = new File(downloadFilepath);
+		
+		HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+		chromePrefs.put("profile.default_content_settings.popups", 0);
+		chromePrefs.put("download.default_directory", file.getAbsolutePath());
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("prefs", chromePrefs);
+		options.addArguments("--start-maximized");
+		//options.addArguments("--headless");
+		options.addArguments("--disable-infobars");
+
+		driver = new ChromeDriver(options);
+		return driver;
+	}
+	
+	public static WebDriver openFFWithOptions() {
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
+		String downloadFilepath = "downloads";
+		File file = new File(downloadFilepath);
+		
+		FirefoxProfile profile = new FirefoxProfile();
+		 
+        // Instructing firefox to use custom download location
+        profile.setPreference("browser.download.folderList", 2);
+ 
+        // Setting custom download directory
+        profile.setPreference("browser.download.dir", file.getAbsolutePath());
+ 
+        // Skipping Save As dialog box for types of files with their MIME
+        profile.setPreference("browser.helperApps.neverAsk.saveToDisk",
+                "text/csv,application/java-archive, application/x-msexcel,application/excel,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml,application/vnd.microsoft.portable-executable");
+ 
+        // Creating FirefoxOptions to set profile
+        FirefoxOptions option = new FirefoxOptions();
+        option.setProfile(profile);
+        // Launching browser with desired capabilities
+
+		driver = new FirefoxDriver(option);
+		return driver;
+	}
+	
 	public static WebDriver openBrowser(String browser) {
 
 		WebDriver driver;
@@ -31,21 +86,12 @@ public class OpenBrowsers {
 
 		return driver;
 	}
-	
-	public static void main(String[] args) throws InterruptedException {
-		String[] browsers = {"chrome","firefox","edge"};
-		for(int i =0;i<browsers.length;i++) {
-			WebDriver driver = OpenBrowsers.openBrowser(browsers[i]);
-			driver.manage().window().maximize();
-			driver.get("https://www.google.com/");
-			Thread.sleep(5000);
-			driver.get("https://www.bing.com/");
-			Thread.sleep(5000);
-			driver.navigate().back();
-			Thread.sleep(5000);
-			driver.navigate().forward();
-			Thread.sleep(10000);
-			driver.quit();
-		}
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		WebDriver driver = OpenBrowsers.openFFWithOptions();
+		driver.get("https://people.sc.fsu.edu/~jburkardt/data/csv/csv.html");
+		TakeScreenShot takeSc = new TakeScreenShot(driver);
+		takeSc.takeScreenShot("downloads/Test.jpg");
+		driver.findElement(By.linkText("cities.csv")).click();
 	}
 }
