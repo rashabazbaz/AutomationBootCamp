32a3db2aa0eebae8c01636688a2f4dd66ce47e1b
Rasha Bazbaz <rbazba@asaltech.com>
2022-10-29 11:03:23 +0300

    appium

--- Appium_with_python/core/ConnectToDevice.py
+++ Appium_with_python/core/ConnectToDevice.py
@@ -9,19 +9,14 @@ class ConnectToDevice:
     @staticmethod
     def connect_to_device(deviceName, apk_file,appPackage, appActivity ):
         desired_caps = {}
-        options = UiAutomator2Options()
-        options.device_name = deviceName
         desired_caps['platformName'] = 'Android'
         desired_caps['deviceName'] = deviceName
 
 
         if apk_file:
-            options.app = os.path.abspath(apk_file)
             desired_caps['app'] = os.path.abspath(apk_file)
         desired_caps["appPackage"] = appPackage
-        options.app_package = appPackage
         if appActivity:
-            options.app_activity = appActivity
             desired_caps['appActivity'] = appActivity
 
         driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
--- Appium_with_python/test/TestCalculator.py
+++ Appium_with_python/test/TestCalculator.py
@@ -9,7 +9,7 @@ from core.ConnectToDevice import ConnectToDevice
 class TestCalculator(unittest.TestCase):
 
     def test(self):
-        driver = ConnectToDevice.connect_to_device("1C171FDEE00AUC", "../apk/sample_Calculator_1.0_Apkpure.apk",
+        driver = ConnectToDevice.connect_to_device("emulator-5554", "../apk/sample_Calculator_1.0_Apkpure.apk",
                                                    "com.bak.mnr.calculatrice", None)
         time.sleep(5)
         element = driver.find_element(AppiumBy.ID,"com.bak.mnr.calculatrice:id/btn2")
